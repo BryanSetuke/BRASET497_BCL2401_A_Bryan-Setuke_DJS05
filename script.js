7f3b3dd3b66715808e6b501f66fee014eb4b8024
BryanSetuke <156883514+BryanSetuke@users.noreply.github.com>
2024-05-24 16:17:36 +0200

    commit

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 import { actionTypes } from "./action.js";
 import { reducer } from "./reducer.js";
 import { createStore } from "./store.js";
+
+// Create the store using the reducer
+const store = createStore(reducer);
