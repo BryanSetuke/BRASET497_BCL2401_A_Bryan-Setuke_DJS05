e39189eb957c37a1815f5f76583a4c2dc81bb088
BryanSetuke <156883514+BryanSetuke@users.noreply.github.com>
2024-05-24 16:18:20 +0200

    commit

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ import { createStore } from "./store.js";
 
 // Create the store using the reducer
 const store = createStore(reducer);
+
+// Subscribe to state changes and log them to the console
+store.subscribe(() => {
+    console.log('State updated:', store.getState());
+});
